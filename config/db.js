50b30b7e701607fb24ff26e3af1b994e23e6c89e
Brostouch KOK <brostouchkokit1007@gmail.com>
2025-06-13 20:57:24 +0700

    connect to mongoDB

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URL);
     console.log(`Connected to Database ${mongoose.connection.host}`.bgWhite);
   } catch (error) {
-    console.log("DB Error", error);
+    console.log("DB Error", error,colors.bgRed);
   }
 };
 
