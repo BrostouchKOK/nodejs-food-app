cbd52ce9807b2a9495f4f544b01b7b3abf288763
Brostouch KOK <brostouchkokit1007@gmail.com>
2025-06-13 18:20:33 +0700

    plugin dependencies

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+// dot env configuration
+dotenv.config();
 
 // rest object
 const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
 
 //rout
 // URL => http://localhost:8080
@@ -10,9 +23,9 @@ app.get("/",(req,res)=>{
 })
 
 // PORT
-const PORT = 8080;
+const PORT = process.env.PORT || 5000
 
 // listen
 app.listen(PORT,()=>{
-    console.log("Server Running");
+    console.log(`Server run on ${PORT}`.white.bgMagenta)
 })
